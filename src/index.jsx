832506123d82fac168d1b6a4a7d5ff33ced58af6
soasme <soasme@gmail.com>
2018-08-15 15:12:29 +1200

    add animated box.

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,60 @@
 import React, {Component} from 'react';
 import blessed from 'blessed';
 import {render} from 'react-blessed';
+import AnimatedBox from './components/AnimatedBox';
 
-// Rendering a simple centered box
-class App extends Component {
+class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+    };
+
+    this.submit = data => this.setState(state => ({name: data}));
+    this.cancel = _ => console.log('Form canceled');
+  }
   render() {
     return (
-      <box top="center"
-           left="center"
-           width="50%"
-           height="50%"
-           border={{type: 'line'}}
-           style={{border: {fg: 'blue'}}}>
-        Hello World!
-      </box>
+      <form
+        keys
+        vi
+        focused
+        onSubmit={this.submit}
+        onReset={this.cancel}
+        left="5%"
+        top="5%"
+        width="90%"
+        height="90%"
+        border={{type: 'line'}}
+        style={{bg: 'cyan', border: {fg: 'blue'}}}
+      >
+        <box width={6} height={3}>Name: </box>
+        <textbox
+          onSubmit={this.submit}
+          left={6}
+          height={3}
+          keys
+          mouse
+          inputOnFocus
+        />
+        <box top={3} height={3}>
+          {`Result: ${this.state.name}`}
+        </box>
+        <AnimatedBox />
+      </form>
     );
   }
 }
 
-// Creating our screen
 const screen = blessed.screen({
   autoPadding: true,
-  smartCSR: true,
-  title: 'react-blessed hello world'
+  // smartCSR: true,
+  title: 'react-blessed form example'
 });
 
-// Adding a way to quit the program
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
 
-// Rendering the React app using our screen
-const component = render(<App />, screen);
+render(<Form />, screen, (inst) => console.log('Rendered Form!'));
